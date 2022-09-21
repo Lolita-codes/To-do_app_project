dfb984b048f611cbf7267c7185988429ec229c44
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2022-09-21 17:13:40 +0100

    Add Views and styling

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add-item', views.add_item, name='add_item'),
+    path('complete/<todo_id>', views.completed_item, name='completed'),
+    path('delete-completed', views.delete_completed, name='delete_completed'),
+    path('delete-all', views.delete_all, name='delete_all')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from .models import ToDo
+from .forms import ToDoForm
 
 
 def index(request):
-    return render(request, 'index.html')
+    form = ToDoForm()
+    todo_list = ToDo.objects.order_by('id')
+    return render(request, 'todo/index.html', {'todo_list': todo_list, 'form': form})
+
+
+@require_POST
+def add_item(request):
+    form = ToDoForm(request.POST)
+    if form.is_valid():
+        new_item = ToDo(item=request.POST['item'])
+        new_item.save()
+
+    return redirect('index')
+
+
+def completed_item(request, todo_id):
+    task = ToDo.objects.get(pk=todo_id)
+    task.completed = True
+    task.save()
+    return redirect('index')
+
+
+def delete_completed(request):
+    all_completed = ToDo.objects.filter(completed=True).delete()
+    return redirect('index')
+
+
+def delete_all(request):
+    all_items = ToDo.objects.all().delete()
+    return redirect('index')
